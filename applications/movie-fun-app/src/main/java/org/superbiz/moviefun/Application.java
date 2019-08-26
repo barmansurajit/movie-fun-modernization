ca75c1367a147e1484331662e0c0a64c609b8adb
Surajit Barman <Surajit.Barman@cognizant.com>
2019-08-25 21:20:19 -0500

    PAL - Breaking Monolith into Components

---
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
@@ -0,0 +1,45 @@
+package org.superbiz.moviefun;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.StorageOptions;
+import org.apache.tika.io.IOUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.superbiz.moviefun.blobstore.BlobStore;
+import org.superbiz.moviefun.blobstore.CloudStorageStore;
+import org.superbiz.moviefun.movies.MovieServlet;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean actionServletRegistration(MovieServlet movieServlet) {
+        return new ServletRegistrationBean(movieServlet, "/moviefun/*");
+    }
+
+    @Bean
+    ServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapServices) {
+        return new ServiceCredentials(vcapServices);
+    }
+
+    @Bean
+    public BlobStore blobStore(ServiceCredentials serviceCredentials) {
+        try {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(IOUtils.toInputStream(serviceCredentials.getCredentials("photo-storage", "user-provided")));
+            return new CloudStorageStore(StorageOptions.newBuilder().setCredentials(credentials).build().getService(), "moviefun-1");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+}
