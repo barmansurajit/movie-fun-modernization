47e670d826dda994e6f37262a2a2a0dc29fb7268
Surajit Barman <Surajit.Barman@cognizant.com>
2019-08-30 20:42:40 -0500

    PAL - Security Modernization

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
@@ -1,8 +1,8 @@
 package org.superbiz.moviefun;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.StorageOptions;
+import org.apache.tika.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,8 +10,10 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.superbiz.cloudfoundry.ServiceCredentials;
 import org.superbiz.moviefun.blobstore.BlobStore;
-import org.superbiz.moviefun.blobstore.S3Store;
+import org.superbiz.moviefun.blobstore.CloudStorageStore;
 import org.superbiz.moviefun.moviesapi.MovieServlet;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
@@ -31,21 +33,14 @@ public class Application {
     }
 
     @Bean
-    public BlobStore blobStore(
-        ServiceCredentials serviceCredentials,
-        @Value("${vcap.services.photo-storage.credentials.endpoint:#{null}}") String endpoint
-    ) {
-        String photoStorageAccessKeyId = serviceCredentials.getCredential("photo-storage", "user-provided", "access_key_id");
-        String photoStorageSecretKey = serviceCredentials.getCredential("photo-storage", "user-provided", "secret_access_key");
-        String photoStorageBucket = serviceCredentials.getCredential("photo-storage", "user-provided", "bucket");
-
-        AWSCredentials credentials = new BasicAWSCredentials(photoStorageAccessKeyId, photoStorageSecretKey);
-        AmazonS3Client s3Client = new AmazonS3Client(credentials);
-
-        if (endpoint != null) {
-            s3Client.setEndpoint(endpoint);
+    public BlobStore blobStore(ServiceCredentials serviceCredentials) {
+        try {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(IOUtils.toInputStream(serviceCredentials.getCredentials("photo-storage", "user-provided")));
+            return new CloudStorageStore(StorageOptions.newBuilder().setCredentials(credentials).build().getService(), "moviefun-1");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return null;
 
-        return new S3Store(s3Client, photoStorageBucket);
     }
 }
